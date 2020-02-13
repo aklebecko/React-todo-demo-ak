b44fbf0d3cd9dae8c68d9a4b90873eab8dddf671
Arpad Klebecko <arpadk120@gmail.com>
2020-02-13 19:40:16 +1100

    Updates

---
+++ precache-manifest.aa96519bbdbf271c1f7b30fdd64edc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e136d08a4146c01346ce8ca6f7cc090",
+    "url": "/aklebecko/React-todo-demo-ak/index.html"
+  },
+  {
+    "revision": "53a4f05aa11cce2a7f79",
+    "url": "/aklebecko/React-todo-demo-ak/static/css/main.557b367b.chunk.css"
+  },
+  {
+    "revision": "623c353dcb037609ef20",
+    "url": "/aklebecko/React-todo-demo-ak/static/js/2.2d0f9acd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/aklebecko/React-todo-demo-ak/static/js/2.2d0f9acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a4f05aa11cce2a7f79",
+    "url": "/aklebecko/React-todo-demo-ak/static/js/main.f3902113.chunk.js"
+  },
+  {
+    "revision": "d6c8533c393a906f4497",
+    "url": "/aklebecko/React-todo-demo-ak/static/js/runtime-main.b7167a1e.js"
+  }
+]);
